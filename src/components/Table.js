c08cfe2f59120ecef9240bfbd86314013d66168f
asafworld <gabrielasaf10@gmail.com>
2022-06-07 17:02:38 -0300

    Corrige falta de uma tr na thead da table. Cumpre requisito 1 do projeto

--- src/components/Table.js
+++ src/components/Table.js
@@ -7,19 +7,21 @@ function Table() {
   return (
     <table>
       <thead>
-        <th>Name</th>
-        <th>Rotation Period</th>
-        <th>Orbital Period</th>
-        <th>Diameter</th>
-        <th>Climate</th>
-        <th>Gravity</th>
-        <th>Terrain</th>
-        <th>Surface Water</th>
-        <th>Population</th>
-        <th>Films</th>
-        <th>Created</th>
-        <th>Edited</th>
-        <th>URL</th>
+        <tr>
+          <th>Name</th>
+          <th>Rotation Period</th>
+          <th>Orbital Period</th>
+          <th>Diameter</th>
+          <th>Climate</th>
+          <th>Gravity</th>
+          <th>Terrain</th>
+          <th>Surface Water</th>
+          <th>Population</th>
+          <th>Films</th>
+          <th>Created</th>
+          <th>Edited</th>
+          <th>URL</th>
+        </tr>
       </thead>
       <tbody>
         {
